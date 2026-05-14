4f2e59c80b137a34ee3bb96955918f838fefe6cf
Sejeong Kim <sejeonggim68@gmail.com>
2026-05-14 17:34:20 +0900

    🔧 Chore: effect 세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-black tracking-tight text-main1">Jumoney</h1>
-      <p className="mt-4 text-zinc-600 dark:text-zinc-400">
-        Welcome to your new project.
-      </p>
-    </main>
-  );
-}
+import React from "react";
+
+const page = () => {
+  return <div>page</div>;
+};
+
+export default page;
