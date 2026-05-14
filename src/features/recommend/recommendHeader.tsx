ec7e90dba8d1834fdee84469a1faede43a2723e5
HyunwooDoo <engusdn0421@gmail.com>
2026-05-14 19:43:28 +0900

    ✨ Feat: 오늘의 호주머니 헤더 구현

---
+++ src/features/recommend/recommendHeader.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const RecommendHeader = () => {
+  return (
+    <div className="bg-primary h-[10.375rem] w-[calc(100%+2rem)] -mx-[1rem] -mt-[1rem] rounded-b-[2.5rem]" />
+  );
+};
+
+export default RecommendHeader;
