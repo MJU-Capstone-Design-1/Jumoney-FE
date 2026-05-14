c446c05271cd4643bfd4d55106fd64af159b560e
Sejeong Kim <sejeonggim68@gmail.com>
2026-05-14 19:43:17 +0900

    ✨ Feat: 바텀 버튼 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import BackButtonField from "@/components/backButtonField";
+import BottomButton from "@/components/bottomButton";
 import React from "react";
 
 const page = () => {
   return (
-    <div>
+    <div className="relative min-h-screen pb-[7rem]">
       <BackButtonField
         color="secondary2"
         label="원하시는 기능을 선택해주세요"
       />
+      <BottomButton disabled={false} label="선택하기" />
     </div>
   );
 };
