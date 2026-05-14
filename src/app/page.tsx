a90d0c51ddcfc066b3a6ebd747a58334a7606d30
Sejeong Kim <sejeonggim68@gmail.com>
2026-05-14 20:53:21 +0900

    🐛 Fix: 테스트 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,7 @@ import BottomButton from "@/components/bottomButton";
 import React from "react";
 
 const page = () => {
-  return (
-    <div className="relative min-h-screen pb-[7rem]">
-      <BackButtonField
-        color="secondary2"
-        label="원하시는 기능을 선택해주세요"
-      />
-      <BottomButton disabled={false} label="선택하기" />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default page;
