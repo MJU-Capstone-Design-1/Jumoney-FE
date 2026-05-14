68a141f7676cab7afe20e2d8fab56f8b44249c3e
Sejeong Kim <sejeonggim68@gmail.com>
2026-05-14 16:21:21 +0900

    🔧 Chore: 디자인 시스템 세팅

---
+++ tailwind.config.ts
@@ -0,0 +1,53 @@
+import type { Config } from "tailwindcss";
+
+const config = {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: "var(--primary)",
+        secondary: {
+          1: "var(--secondary-1)",
+          2: "var(--secondary-2)",
+        },
+        main: {
+          1: "var(--main-1)",
+          2: "var(--main-2)",
+          3: "var(--main-3)",
+          4: "var(--main-4)",
+        },
+        sub: {
+          1: "var(--sub-1)",
+          2: "var(--sub-2)",
+          3: "var(--sub-3)",
+          4: "var(--sub-4)",
+        },
+        text: {
+          main: "var(--text-main)",
+          sub: "var(--text-sub)",
+          up: "var(--text-up)",
+          down: "var(--text-down)",
+        },
+        default: "var(--default)",
+        active: "var(--active)",
+        inactive: "var(--inactive)",
+        field: {
+          it: "var(--field-it)",
+          mobility: "var(--field-mobility)",
+          finance: "var(--field-finance)",
+          bio: "var(--field-bio)",
+          steal: "var(--field-steal)",
+          energy: "var(--field-energy)",
+          communication: "var(--field-communication)",
+          staples: "var(--field-staples)",
+          mechanic: "var(--field-mechanic)",
+          utility: "var(--field-utility)",
+        },
+      },
+    },
+  },
+} satisfies Config;
+
+export default config;
