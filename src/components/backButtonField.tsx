2f50a0fca5f92236b7fdeb873ff5f12bdc9e903d
Sejeong Kim <sejeonggim68@gmail.com>
2026-05-15 18:18:56 +0900

    ✨ Feat: 뒤로 가기 버튼 라우팅 로직 구현

--- src/components/backButtonField.tsx
+++ src/components/backButtonField.tsx
@@ -3,9 +3,10 @@ import BackButtonIcon from "./icons/backButtonIcon";
 interface BackButtonFieldProps {
   color?: "secondary1" | "secondary2";
   label?: string;
+  onClick?: () => void;
 }
 
-const BackButtonField = ({ color, label }: BackButtonFieldProps) => {
+const BackButtonField = ({ color, label, onClick }: BackButtonFieldProps) => {
   const colorMap = {
     secondary1: "text-secondary1",
     secondary2: "text-secondary2",
@@ -14,7 +15,10 @@ const BackButtonField = ({ color, label }: BackButtonFieldProps) => {
   const textColorClass = (color && colorMap[color]) || "text-secondary2";
 
   return (
-    <div className={`flex items-center gap-[0.75rem] ${textColorClass}`}>
+    <div
+      onClick={onClick}
+      className={`flex cursor-pointer items-center gap-[0.75rem] ${textColorClass}`}
+    >
       <BackButtonIcon color={color} />
       <div className="text-body-xl font-extrabold">{label}</div>
     </div>
