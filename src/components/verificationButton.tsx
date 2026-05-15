5daead11c6e1e85d2502d5074d0b1f0e7b620c42
HyunwooDoo <engusdn0421@gmail.com>
2026-05-16 02:15:39 +0900

    ✨ Feat: 검증하기 버튼 공용 컴포넌트 구현

---
+++ src/components/verificationButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const VerificationButton = () => {
+  return (
+    <button className="flex w-[6rem] text-text-main h-[2rem] rounded-[6.25rem] bg-default text-body-sm font-semibold justify-center items-center">
+      과거 지표 검증하기
+    </button>
+  );
+};
+
+export default VerificationButton;
