dfd649e63b31b7daa64d0b16c129d9e3afd0f64e
Sejeong Kim <sejeonggim68@gmail.com>
2026-05-16 23:27:26 +0900

    🐛 Fix: 플로팅 버튼 모션 중 깜빡이는 오류 수정

--- src/features/recommend/selection/floatingButton.tsx
+++ src/features/recommend/selection/floatingButton.tsx
@@ -1,22 +1,28 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import CheckIcon from '@/components/icons/checkIcon';
-import { motion, useScroll, useSpring } from 'framer-motion';
+import { motion, useScroll, useSpring, useTransform } from 'framer-motion';
 
 export default function FloatingButton({ onClick }: { onClick?: () => void }) {
   const { scrollY } = useScroll();
+
   const springY = useSpring(scrollY, {
     stiffness: 60,
     damping: 15,
     mass: 0.5,
   });
 
+  const transformY = useTransform(springY, (value) => value);
+
   return (
     <motion.button
       onClick={onClick}
-      style={{ y: springY }}
-      className='bg-secondary2 shadow-card-shadow fixed top-3 right-6 z-50 flex h-[3.5rem] w-[3.5rem] items-center justify-center rounded-full'
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ type: 'spring', stiffness: 260, damping: 15, delay: 0.5 }}
+      style={{ y: transformY }}
+      className='bg-secondary2 shadow-card-shadow fixed top-3 right-6 z-[90] flex h-[3.5rem] w-[3.5rem] items-center justify-center rounded-full'
     >
       <CheckIcon />
     </motion.button>
