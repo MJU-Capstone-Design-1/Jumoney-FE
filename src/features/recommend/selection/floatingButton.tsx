a9b0419d12166565632ed12cd7387d490c5f13a2
Sejeong Kim <sejeonggim68@gmail.com>
2026-05-17 00:37:40 +0900

    🐛 Fix: 플로팅 버튼 따라오는 모션 제거 후 위치 고정

--- src/features/recommend/selection/floatingButton.tsx
+++ src/features/recommend/selection/floatingButton.tsx
@@ -2,27 +2,20 @@
 
 import React from 'react';
 import CheckIcon from '@/components/icons/checkIcon';
-import { motion, useScroll, useSpring, useTransform } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 export default function FloatingButton({ onClick }: { onClick?: () => void }) {
-  const { scrollY } = useScroll();
-
-  const springY = useSpring(scrollY, {
-    stiffness: 60,
-    damping: 15,
-    mass: 0.5,
-  });
-
-  const transformY = useTransform(springY, (value) => value);
-
   return (
     <motion.button
       onClick={onClick}
       initial={{ opacity: 0, scale: 0 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ type: 'spring', stiffness: 260, damping: 15, delay: 0.5 }}
-      style={{ y: transformY }}
-      className='bg-secondary2 shadow-card-shadow fixed top-3 right-6 z-[90] flex h-[3.5rem] w-[3.5rem] items-center justify-center rounded-full'
+      style={{
+        left: '50%',
+        marginLeft: 'calc(187.5px - 3.5rem - 1.5rem)',
+      }}
+      className='bg-secondary2 shadow-card-shadow pointer-events-auto fixed top-3 z-[999] flex h-[3.5rem] w-[3.5rem] items-center justify-center rounded-full'
     >
       <CheckIcon />
     </motion.button>
