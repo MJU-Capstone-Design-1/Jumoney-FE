43bdcf5b3d8a7410be793d1ac581f5415c9595da
HyunwooDoo <engusdn0421@gmail.com>
2026-05-17 23:04:47 +0900

    ✨ Feat: 오른쪽 방향 화살표 아이콘 구현

---
+++ src/components/icons/rightArrowIcon.tsx
@@ -0,0 +1,25 @@
+export const RightArrowIcon = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='12'
+      height='13'
+      viewBox='0 0 12 13'
+      fill='none'
+      className='size-3'
+    >
+      <path
+        d='M1 6.00781L10.5 6.00781'
+        stroke='currentColor'
+        strokeWidth='2'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M7.16469 10.3602C8.12154 10.1035 8.96705 9.53779 9.57009 8.75086C10.1731 7.96394 10.5 6.99975 10.5 6.00785C10.5 5.01595 10.1731 4.05176 9.57009 3.26483C8.96705 2.4779 8.12154 1.91221 7.16469 1.65549'
+        stroke='currentColor'
+        strokeWidth='2'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
