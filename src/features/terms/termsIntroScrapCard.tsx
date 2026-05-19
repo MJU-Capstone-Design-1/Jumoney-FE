065361c3bfcd00993df84107b84963d47cb7793a
HyunwooDoo <engusdn0421@gmail.com>
2026-05-19 13:01:34 +0900

    ✨ Feat: 인트로 스크랩 카드 구현

---
+++ src/features/terms/termsIntroScrapCard.tsx
@@ -0,0 +1,12 @@
+import { ScrapIcon } from '@/components/icons/scrapIcon';
+
+export const TermsIntroScrapCard = () => {
+  return (
+    <div className='bg-secondary1 shadow-card-shadow flex items-center gap-[0.5rem] rounded-[1.5rem] p-[1rem]'>
+      <div className='bg-background flex h-[3rem] w-[3rem] items-center justify-center rounded-[1rem]'>
+        <ScrapIcon />
+      </div>
+      <p className='text-label-sm font-extrabold'>용어이름</p>
+    </div>
+  );
+};
