910e5406d79f2e519e9b04e6803d7854616207c8
HyunwooDoo <engusdn0421@gmail.com>
2026-05-20 10:26:43 +0900

    ✨ Feat: 용어 인트로 페이지 스크랩 카드 모션 구현

--- src/features/terms/termsIntroScrapSection.tsx
+++ src/features/terms/termsIntroScrapSection.tsx
@@ -1,11 +1,23 @@
+'use client';
+
 import { CommonButton } from '@/components/commonButton';
 import { RightArrowIcon } from '@/components/icons/rightArrowIcon';
 import { TermsIntroScrapCard } from './termsIntroScrapCard';
+import { motion } from 'framer-motion';
 
 export const TermsIntroScrapSection = () => {
   return (
     <div className='flex flex-col gap-[1rem]'>
-      <div className='flex justify-between'>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.8,
+          duration: 0.5,
+          ease: 'easeOut',
+        }}
+        className='flex items-center justify-between'
+      >
         <p className='text-label-md font-extrabold'>스크랩한 용어</p>
         <CommonButton
           href='/terms/scrap'
@@ -14,12 +26,23 @@ export const TermsIntroScrapSection = () => {
         >
           전체보기
         </CommonButton>
-      </div>
+      </motion.div>
 
-      <div className='grid grid-cols-2 gap-[1rem]'>
+      <motion.div
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          delay: 1.2,
+          duration: 0.5,
+          type: 'spring',
+          stiffness: 200,
+          damping: 20,
+        }}
+        className='grid grid-cols-2 gap-[1rem]'
+      >
         <TermsIntroScrapCard />
         <TermsIntroScrapCard />
-      </div>
+      </motion.div>
     </div>
   );
 };
