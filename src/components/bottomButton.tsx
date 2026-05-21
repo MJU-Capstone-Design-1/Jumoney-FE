d866ea68f5dcca5c4984c8959ffdf213fea9f5e2
HyunwooDoo <engusdn0421@gmail.com>
2026-05-21 18:00:46 +0900

    🎨 Design: bottomButton 배경 optional 추가

--- src/components/bottomButton.tsx
+++ src/components/bottomButton.tsx
@@ -2,19 +2,23 @@ interface BottomButtonProps {
   label: string;
   onClick?: () => void;
   disabled?: boolean;
+  bgColor?: string;
+  textColor?: string;
 }
 
 const BottomButton = ({
   label,
   onClick,
   disabled = false,
+  bgColor = 'bg-secondary2',
+  textColor = 'text-secondary1',
 }: BottomButtonProps) => {
   return (
     <button
       onClick={onClick}
       disabled={disabled}
-      className={`text-secondary1 fixed bottom-[2.125rem] left-1/2 flex h-[4rem] w-[21.4375rem] -translate-x-1/2 items-center justify-center gap-[2rem] rounded-[1000px] px-[2rem] py-[1rem] transition-colors ${
-        disabled ? 'bg-default' : 'bg-secondary2'
+      className={`fixed bottom-[2.125rem] left-1/2 flex h-[4rem] w-[21.4375rem] -translate-x-1/2 items-center justify-center gap-[2rem] rounded-[1000px] px-[2rem] py-[1rem] transition-colors ${textColor} ${
+        disabled ? 'bg-default' : bgColor
       }`}
     >
       <span className='text-body-xl font-extrabold'>{label}</span>
