4d3c118503e563218ba878ab33df3cc3c101f76c
HyunwooDoo <engusdn0421@gmail.com>
2026-05-21 18:01:06 +0900

    ✨ Feat: termsId 페이지 구현

--- src/app/(main)/terms/[termsSectionId]/[termsId]/page.tsx
+++ src/app/(main)/terms/[termsSectionId]/[termsId]/page.tsx
@@ -1,7 +1,43 @@
+import BackButtonField from '@/components/backButtonField';
+import BottomButton from '@/components/bottomButton';
+import { TermsScrapButton } from '@/features/terms/termsScrapButton';
+
 import React from 'react';
 
 const page = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className='flex flex-col gap-[2rem] p-[1rem]'>
+        <div className='flex items-center justify-between'>
+          <BackButtonField color='secondary2' label='기초 개념' />
+          <div className='right-0 px-[0.375rem]'>
+            <TermsScrapButton />
+          </div>
+        </div>
+
+        <div className='bg-default mx-auto h-[18.75rem] w-[18.75rem]' />
+      </div>
+
+      <div className='bg-secondary1 shadow-card-shadow flex h-[60rem] w-[60rem] translate-x-[-18.28125rem] flex-col items-center gap-[1rem] rounded-[77.125rem] pt-[2.25rem]'>
+        <h1 className='text-heading-md text-main2 leading-[120%] font-extrabold'>
+          매수
+        </h1>
+        <div className='flex flex-col gap-[0.5rem] text-center'>
+          <p className='text-label-md leading-[120%] font-extrabold'>
+            주식을 사는 행위
+          </p>
+          <p className='text-body-lg text-text-main leading-[120%] font-semibold'>
+            매수란 주식을 사는 것을 말해요.
+            <br />
+            회사의 주식을 판매하는 사람을 매도자라고 하는데, <br />
+            매도자에게 주식을 구매하는 행위에요.
+          </p>
+        </div>
+      </div>
+
+      <BottomButton bgColor='bg-main2 ' label='다음으로' />
+    </div>
+  );
 };
 
 export default page;
