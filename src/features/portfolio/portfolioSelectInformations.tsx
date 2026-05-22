ec17e29d7f84d0aa27d1e4ceadbd82d9d85e5ffc
HyunwooDoo <engusdn0421@gmail.com>
2026-05-22 23:11:36 +0900

    ✨ Feat: 거장 기본 정보 구현

---
+++ src/features/portfolio/portfolioSelectInformations.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+export const MASTERS = [
+  {
+    name: '워런 버핏',
+    image: '/images/warrenBuffetImage.svg',
+    bgColor: 'bg-main1',
+    tags: ['가치 투자', '경제적 해자'],
+    quote: (
+      <>
+        규칙 1: 절대 돈을 잃지 마라
+        <br />
+        규칙 2: 규칙 1을 잊지 마라
+      </>
+    ),
+  },
+  {
+    name: '피터 린치',
+    image: '/images/peterLynchImage.svg',
+    bgColor: 'bg-main2',
+    tags: ['성장주 투자', '생활 속 발견'],
+    quote: (
+      <>
+        자신이 이해할 수 있는 기업에 투자하라
+        <br />
+        기업이 무엇을 하는지 찾아내라
+      </>
+    ),
+  },
+  {
+    name: '레이 달리오',
+    image: '/images/rayDalioImage.svg',
+    bgColor: 'bg-main3',
+    tags: ['거시 투자', '올웨더 포트폴리오'],
+    quote: (
+      <>
+        가장 원하는 목표를 선택하고 헌신하세요
+        <br />
+        일반 대중들이 말하는 것과 반대로 하세요
+      </>
+    ),
+  },
+  {
+    name: '윌리엄 오닐',
+    image: '/images/williamOneilImage.svg',
+    bgColor: 'bg-main4',
+    tags: ['모멘텀 투자', 'CAN SLIM 전략'],
+    quote: (
+      <>
+        가장 큰 실수는 이미 하락한 주식을
+        <br /> 더 싸게 사려고 하는 것이다
+      </>
+    ),
+  },
+];
