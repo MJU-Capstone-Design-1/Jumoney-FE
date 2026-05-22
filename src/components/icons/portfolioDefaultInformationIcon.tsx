787b6eaee033195f18c5f8185d8d2d94f7641758
HyunwooDoo <engusdn0421@gmail.com>
2026-05-23 01:45:12 +0900

    ✨ Feat: 기본/상세 포트폴리오 버튼 아이콘 구현

---
+++ src/components/icons/portfolioDefaultInformationIcon.tsx
@@ -0,0 +1,47 @@
+export const PortfolioDefaultInformationIcon = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+    >
+      <path
+        d='M3 5H11'
+        stroke='white'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M3 12H11'
+        stroke='white'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M3 19H11'
+        stroke='white'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M15 5L18 8L21 5'
+        stroke='white'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M15 19L18 16L21 19'
+        stroke='white'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
