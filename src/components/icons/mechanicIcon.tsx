cf7b633a80cf1b1a8c9875663db403144e7a3638
Sejeong Kim <sejeonggim68@gmail.com>
2026-05-23 08:22:41 +0900

    ✨ Feat: 섹터 10개 아이콘 추가

---
+++ src/components/icons/mechanicIcon.tsx
@@ -0,0 +1,47 @@
+export const MechanicIcon = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+    >
+      <path
+        d='M12 10.1895V14.0005'
+        stroke='white'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M12 2V5'
+        stroke='white'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M19 13V7C19 6.46957 18.7893 5.96086 18.4142 5.58579C18.0391 5.21071 17.5304 5 17 5H7C6.46957 5 5.96086 5.21071 5.58579 5.58579C5.21071 5.96086 5 6.46957 5 7V13'
+        stroke='white'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M19.3796 19.9997C20.4553 18.1843 21.0154 16.1099 20.9996 13.9997L12.8116 10.3607C12.5559 10.2472 12.2793 10.1885 11.9996 10.1885C11.7199 10.1885 11.4432 10.2472 11.1876 10.3607L2.99959 13.9997C2.95329 16.8434 3.9533 19.605 5.80958 21.7597'
+        stroke='white'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M2 21C2.6 21.5 3.2 22 4.5 22C7 22 7 20 9.5 20C10.8 20 11.4 20.5 12 21C12.6 21.5 13.2 22 14.5 22C17 22 17 20 19.5 20C20.8 20 21.4 20.5 22 21'
+        stroke='white'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
