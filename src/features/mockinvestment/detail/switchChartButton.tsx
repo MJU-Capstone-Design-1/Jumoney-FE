95df4dc43a602df8cdc7a33bc9bebac1443c64db
Sejeong Kim <sejeonggim68@gmail.com>
2026-05-24 01:13:45 +0900

    ✨ Feat: 차트 변경 버튼 구현

---
+++ src/features/mockinvestment/detail/switchChartButton.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { ChartIcon } from '@/components/icons/chartIcon';
+import { GraphIcon } from '@/components/icons/graphIcon';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+
+export const SwitchChartButton = () => {
+  const [isChart, setIsChart] = useState(false);
+
+  const handleToggle = () => {
+    setIsChart((prev) => !prev);
+  };
+
+  return (
+    <motion.button
+      type='button'
+      onClick={handleToggle}
+      whileTap={{ scale: 0.85 }}
+      transition={{ type: 'spring', stiffness: 400, damping: 17 }}
+      className={`border-secondary2 flex h-10 min-h-10 w-10 min-w-10 shrink-0 items-center justify-center rounded-full border outline-none ${
+        isChart ? 'bg-secondary2' : 'bg-secondary1'
+      }`}
+    >
+      {isChart ? <ChartIcon /> : <GraphIcon />}
+    </motion.button>
+  );
+};
