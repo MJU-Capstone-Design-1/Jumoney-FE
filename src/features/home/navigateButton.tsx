ec7db79e93647d2245e1040f0c9fd31a8cd6668d
Sejeong Kim <sejeonggim68@gmail.com>
2026-05-24 17:00:15 +0900

    ✨ Feat: 내비게이션 버튼 내 버튼 기능 추가

--- src/features/home/navigateButton.tsx
+++ src/features/home/navigateButton.tsx
@@ -2,15 +2,20 @@ import { RightArrowIconSmall } from '@/components/icons/rightArrowIconSmall';
 
 interface NavigateButtonProps {
   label: string;
+  onClick?: () => void;
 }
 
-export const NavigateButton = ({ label }: NavigateButtonProps) => {
+export const NavigateButton = ({ label, onClick }: NavigateButtonProps) => {
   return (
-    <div className='bg-secondary2 flex h-[1.875rem] w-[5.25rem] items-center justify-center gap-[0.125rem] rounded-full px-[1rem] py-[0.5rem]'>
-      <div className='text-body-sm text-secondary1 text-center font-bold'>
+    <button
+      type='button'
+      onClick={onClick}
+      className='bg-secondary2 flex h-[2rem] w-[5.25rem] items-center justify-center gap-[0.125rem] rounded-full px-[1rem] py-[0.625rem]'
+    >
+      <div className='text-body-sm text-secondary1 mt-[0.0625rem] text-center font-bold'>
         {label}
       </div>
       <RightArrowIconSmall />
-    </div>
+    </button>
   );
 };
