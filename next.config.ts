3255e114471c0bf434f1c6d5b960188e0bbc1d22
HyunwooDoo <engusdn0421@gmail.com>
2026-06-02 00:09:16 +0900

    🔧 Chore: Next.js Rewrites 경로 추가

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/news/today',
+        destination: 'https://jumoney-node.shop/api/news/today',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
