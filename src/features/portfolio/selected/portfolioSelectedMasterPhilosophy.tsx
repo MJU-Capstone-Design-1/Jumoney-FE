e0a2eb09bdca5ac66614d6ba9ebe3b2f7aef41e8
HyunwooDoo <engusdn0421@gmail.com>
2026-06-01 16:10:03 +0900

    ✨ Feat: 포트폴리오 selected subtitle 간격 줄바꿈 + 본문 상단 padding ',' 기준 추가

--- src/features/portfolio/selected/portfolioSelectedMasterPhilosophy.tsx
+++ src/features/portfolio/selected/portfolioSelectedMasterPhilosophy.tsx
@@ -17,11 +17,17 @@ export const PortfolioSelectedMasterPhilosophy = ({
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.6, ease: 'easeOut', delay: 0.8 }}
-      className='flex flex-col items-center gap-[0.75rem] pt-[1rem]'
+      className='flex flex-col items-center gap-[0.75rem] pt-[2rem]'
     >
       <p className='text-label-md leading-[120%] font-extrabold'>{title}</p>
       <p className='text-body-sm text-center leading-[120%] font-bold'>
-        {description}
+        {description.split(',').map((part, index, array) => (
+          <React.Fragment key={index}>
+            {part.trim()}
+            {index < array.length - 1 && ','}
+            {index < array.length - 1 && <br />}
+          </React.Fragment>
+        ))}
       </p>
     </motion.div>
   );
